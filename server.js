e1ed26de4b669faa89ecd4660bbb7b066c840125
Sivaranjani Jayaprakash <shiva.ranjani83@gmail.com>
2022-07-21 18:55:50 -0400

    route and functionlity for delete note

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const fs = require('fs');
+const uuid = require('./helpers/uuid');
 const { json } = require('express');
+
 
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')));
@@ -23,9 +25,14 @@ app.get('/api/notes', (req, res) => {
 
 //route to add new notes entered to db.json file and display on screen
 app.post('/api/notes', (req, res) => {
+    var savedNotes = [];
+    var content = fs.readFileSync('./db/db.json', 'utf8', (err, data) => { });
 
-    var savedNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8', (err, data) => { }));
-    req.body.id=  savedNotes.length + 1;
+    if (content.length > 0) {
+        savedNotes = JSON.parse(content);
+    }
+
+    req.body.id = uuid();
     savedNotes.push(req.body);
     fs.writeFileSync('./db/db.json', JSON.stringify(savedNotes, '\t'), err => {
         console.log('new note added to file');
@@ -35,6 +42,21 @@ app.post('/api/notes', (req, res) => {
 
 //route to delete the requested notes and display the remaining data 
 
+app.delete('/api/notes/:id', (req, res) => {
+
+
+    var savedNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8', (err, data) => { }));
+ 
+
+    savedNotes = savedNotes.filter(x => x.id != req.params.id);
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(savedNotes, '\t'), err => {
+        console.log('new note added to file');
+    });
+
+  res.send(savedNotes);
+
+});
 
 
 app.listen(PORT, () =>
